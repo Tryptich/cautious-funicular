e8ac214f3744dc57b6b5ef604a394b912132c83a
Steve Ruben <stevetuenkam@gmail.com>
2020-12-29 00:04:05 +0100

    adding header

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -23,7 +23,7 @@ class TopBar extends React.Component {
                         </div>
                         </div>
                     </div>
-                    <div class=" pr-2">
+                    <div class="flex-auto pr-2">
                         <div class="box-wrapper">
                             <div class=" bg-white rounded flex items-center w-full p-3 shadow-sm border border-gray-200">
                                 <button class="outline-none focus:outline-none"><svg class=" w-5 text-gray-600 h-5 cursor-pointer" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></button>
